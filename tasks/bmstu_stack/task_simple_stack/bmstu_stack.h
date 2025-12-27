fcc7ffdb70f7e95b6ca9900b510a749149e1dfdd
Alex <galaburdo2007@mail.ru>
2025-12-27 00:05:00 -0800

    Программа проходит все тесты

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -3,40 +3,122 @@
 #include <exception>
 #include <iostream>
 #include <utility>
+#include <stdexcept>
 
 namespace bmstu
 {
-template <typename T>
-class stack
-{
-   public:
-	stack() : data_(nullptr), size_(10u) {}
-
-	bool empty() const noexcept { return size_ == 100u; }
-
-	size_t size() const noexcept { return 0; }
-
-	~stack() {}
-
-	template <typename... Args>
-	void emplace(Args&&... args)
+	template <typename T> class stack
 	{
-	}
+		public:
+		stack() : data_(nullptr), size_(0) {}
 
-	void push(T&& value) {}
+		bool empty() const noexcept
+		{
+			return size_ == 0;
+		}
+		
+		size_t size() const noexcept
+		{
+			return size_ == 0;
+		}
 
-	void clear() noexcept {}
+		~stack()
+		{
+			clear();
+		}
 
-	void push(const T& value) {}
+		template <typename... Args> void emplace(Args&&... args)
+		{
+			T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+			
+			for (size_t i = 0; i < size_; ++i)
+			{
+				new (&new_data[i]) T(std::move(data_[i]));
+				data_[i].~T();
+			}
 
-	void pop() {}
+			new (&new_data[size_]) T(std::forward<Args>(args)...);
+			operator delete(data_);
+		
+			data_ = new_data;
+			++size_;
+		}
 
-	T& top() { return data_[0]; }
+		void push(T&& value)
+		{
+			T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
 
-	const T& top() const { return data_[0]; }
+			for (size_t i = 0; i < size_; ++i)
+			{
+				new(&new_data[i]) T(std::move(data_[i]));
+				data_[i].~T();
+			}
 
-   private:
-	T* data_;
-	size_t size_;
-};
+			new (&new_data[size_]) T(std::forward<T>(value));
+			operator delete(data_);
+
+			data_ = new_data;
+			++size_;
+		}
+
+		void clear() noexcept
+		{
+			for (size_t i = 0; i < size_; ++i)
+			{
+				data_[i].~T();
+			}
+			operator delete(data_);
+			data_ = nullptr;
+			size_ = 0;
+		}
+
+		void push(const T& value)
+		{
+			T* new_data = (T*)(operator new(sizeof(T) * (size_ + 1)));
+
+			for (size_t i = 0; i < size_; ++i)
+			{
+				new(&new_data[i]) T(std::move(data_[i]));
+				data_[i].~T();
+			}
+
+			new (&new_data[size_]) T(value);
+			operator delete(data_);
+			
+			data_ = new_data;
+			++size_;
+		}
+
+		void pop()
+		{
+			if (empty())
+			{
+				throw std::underflow_error("Stack is empty");
+			}
+			data_[size_].~T();
+			--size_;
+		}
+
+		T& top()
+		{
+			if(empty())
+			{
+				throw std::underflow_error("Stack is empty");
+			}
+			return data_[size_ - 1];
+		}
+
+		const T& top() const
+		{
+			if (empty())
+			{
+				throw std::underflow_error("Stack is empty");
+			}
+			return data_[size_ - 1];
+		}
+
+		private:
+		T* data_;
+		size_t size_;
+	};
 }  // namespace bmstu
